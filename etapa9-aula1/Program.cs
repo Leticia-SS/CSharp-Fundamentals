77fdb45db88e309279ca07307dd1c8ec4766fae1
leticia.ssilva <leticia.saraiva013@gmail.com>
2025-03-26 08:16:34 -0300

    Aula leitura e escrita em .txt

---
+++ etapa9-aula1/Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // StreamReader s = new StreamReader();
+        // StreamWriter w = new StreamWriter();
+
+        string[] diretorios = Directory.GetDirectories(@"C:\");
+
+        string[] arquivos = Directory.GetFiles(@"C:\_pasta","*.*",SearchOption.AllDirectories);
+
+
+        if(File.Exists("\\_pasta\\altera\\output.txt"))
+        {
+            FileInfo inforarquivo = new FileInfo(@"\_pasta\altera\output.txt");
+            Console.WriteLine($"Nome Completo: {inforarquivo.FullName}");
+            Console.WriteLine($"Data de Criação: {inforarquivo.CreationTime}");
+            Console.WriteLine($"Tamanho do Arquivo: {inforarquivo.Length}");
+        }
+
+        string novoDiretorio = @"C:\MeuDiretorio";
+
+        Directory.CreateDirectory(novoDiretorio);
+
+        string nomeArquivo = "MeuArquivo.txt";
+
+        string caminhoArquivo = Path.Combine(novoDiretorio, nomeArquivo);
+
+        using (var fluxoArquivo = File.Create(caminhoArquivo)) 
+        {
+            for(byte i = 0; i < 10; i++)
+            {
+                fluxoArquivo.WriteByte(i);
+            }
+
+        }
+
+        // USANDO STREAMWRITER
+
+        using (var fluxoArquivo = new StreamWriter(caminhoArquivo))
+        {
+            string conteudo = "Texto legível";
+
+            for(int i = 0; i< 10; i++)
+            {
+                fluxoArquivo.WriteLine($"{i} - {conteudo}");
+            }
+
+            fluxoArquivo.WriteLine(conteudo);
+        }
+
+        Console.ReadKey();
+    }
+}
